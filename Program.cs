b2f817ab3d23c857cd25cce916c09d997c43b905
ryuselmi <yuselmirodriguez24@gmail.com>
2024-07-17 09:53:31 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex._6._3_CSC205
+{
+
+    public class SquareRootCalculator
+    {
+
+        public static double SquareRoot(double a)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Cannot compute the square root of a negative number");
+            }
+
+            double x0 = a / 2;
+            double x1 = (x0 + a / x0) / 2;
+
+            while (Math.Abs(x1 - x0) >= 0.0001)
+            {
+                x0 = x1;
+                x1 = (x0 + a / x0) / 2;
+            }
+
+            return x1;
+
+        }
+
+        public static void Main()
+        {
+            double number = 9;
+            double result = SquareRoot(number);
+            Console.WriteLine("Square root of {0} is approximately {1}", number, result);
+        }
+    }
+}
